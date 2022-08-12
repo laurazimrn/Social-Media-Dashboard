0fe7e77c8a9ccc7e5c24bdac948fe449d78b9785
Laura Lacort Zimmermann <81427562+laurazimrn@users.noreply.github.com>
2022-08-12 14:42:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let toggleSwitch = document.getElementById("slider");
+let html = document.querySelector("html");
+
+toggleSwitch.addEventListener("click", () => {
+    toggleSwitch.classList.toggle("dark");
+  html.classList.toggle("dark");
+});
